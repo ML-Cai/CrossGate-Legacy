f7dc729a5ced040f5a133b0cbca48e13606eea0a
Meng-Lun Cai <r7418529@gmail.com>
2025-04-05 04:23:55 +0800

    Add basic cgl::Settings & cgl::SettingsLoader to handle the engine setting (#1)
    - Add basic cgl::Settings & cgl::SettingsLoader to handle the engine setting
    - Add google test (branch v1.16.x) as submodule.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CrossGate-Legacy
+
+This is a implementation of "CrossGate" game.
+
+Reference : https://cgsword.com/filesystem_graphicmap.htm
+
+# Build
+
+See following commands to build the project
+
+      $> git submodule update --recursive --init
+      $> cmake ..
+      $> make -j8
+
---
+++ include/cgl/core/results.h
@@ -0,0 +1,29 @@
+// =============================================================================
+//   The MIT License (MIT)
+//
+//   Copyright (c) 2024-2025 MengLun,Cai
+//
+//   All rights reserved.
+// =============================================================================
+
+#pragma once
+
+namespace cgl {
+
+// -----------------------------------------------------------------------------
+// Results
+// -----------------------------------------------------------------------------
+#define Results_ENUM_LIST              \
+    Results_X(Success)                 \
+    Results_X(Fail)                    \
+    Results_X(OutOfMemory)             \
+    Results_X(InvalidArgs)             \
+    Results_X(UnknownError)
+
+enum class Results : unsigned int {
+#define Results_X(name) name,
+    Results_ENUM_LIST
+#undef Results_X
+};
+
+}   // namespace cgl
