df0b7bb2c9e7df266c9c68c7074cb1f8bdbe9858
Meng-Lun Cai <r7418529@gmail.com>
2025-08-16 02:42:47 -0700

    Restruct requirements as submodule and update the `setup_requirements.py` script to install that (#16)
    Restruct requirements as submodule and update the `setup_requirements.py` script to install that
    
    - Add google / spdlog as submodule
    - Restruct requirements as submodule and update the `setup_requirements.py` script to install that

--- setup_requirements.py
+++ setup_requirements.py
@@ -9,75 +9,86 @@ import os
 import sys
 import subprocess
 import shutil
+import argparse
 
 # ----------------------------------------------------------------------------------------------------------------------
 def run_cmd(cmd, cwd=None):
-    result = subprocess.run(
-        cmd,
-        cwd=cwd,
-        capture_output=True,
-        text=True
-    )
+    print(f"Running command: {cmd}")
+    result = subprocess.run(cmd, cwd=cwd, text=True)
 
     if result.returncode != 0:
         print(result.stderr)
         raise subprocess.CalledProcessError(result.returncode, cmd)
 
 # ----------------------------------------------------------------------------------------------------------------------
-def fetch_repository(dep_name, url, branch, temp_dir):
-    dep_path = os.path.join(temp_dir, dep_name)
+def build_cmake_depend(workspace, build_type, depend_name, *cmake_args):
+    soruce_path  = os.path.join(workspace, "third_party", depend_name)
+    install_path = os.path.join(workspace, "third_party", "build", build_type)
+    build_path   = os.path.join(workspace, "build", "third_party", depend_name)
 
-    if not os.path.exists(dep_path):
-        print(f"-- Fetch {dep_name}:{branch} from `{url}`")
-        run_cmd(["git", "clone", "--depth", "1", "--branch", branch, url, dep_path])
+    print("-------------------------------------------------------------------")
+    print(f"Build depend: {soruce_path} ...")
+    print("-------------------------------------------------------------------")
 
-    return dep_path
+    cmd = [
+        "cmake",
+            "-S", soruce_path,
+            "-B", build_path,
+            f"-DCMAKE_INSTALL_PREFIX={install_path}",
+            f"-DCMAKE_BUILD_TYPE={build_type}"
+        ]
+    cmd.extend(cmake_args)
+    run_cmd(cmd, cwd = soruce_path)
 
-# ----------------------------------------------------------------------------------------------------------------------
-def build_dep(dep_name, install_dir, dep_path, build_type, *cmake_args):
-    cmake_cmd = ["cmake",
-                    "-S", dep_path,
-                    "-B", "build",
-                    f"-DCMAKE_INSTALL_PREFIX={install_dir}",
-                    f"-DCMAKE_BUILD_TYPE={build_type}",]
-    cmake_cmd.extend(cmake_args)
-    run_cmd(cmake_cmd, cwd=dep_path)
-
-    run_cmd(["cmake",
-                "--build", "build",
-                "--target", "install",
-                "--config", build_type,
-            ], cwd=dep_path)
-
-# ----------------------------------------------------------------------------------------------------------------------
-def setup_dep(install_dir, temp_dir, dep_name, url, branch, build_type, *cmake_args):
-    print(f"-- Setup dependency : `{dep_name}:{branch}` ...")
-
-    # fetch source from repository
-    dep_path = fetch_repository(dep_name, url, branch, temp_dir)
-
-    # check build
-    build_dep(dep_name, install_dir, dep_path, build_type, *cmake_args)
+    run_cmd([
+        "cmake",
+            "--build", build_path,
+            "--target", "install",
+            "--config", build_type,
+        ],
+        cwd = soruce_path)
 
 # ----------------------------------------------------------------------------------------------------------------------
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: setup_gtest.py <install_dir> <temp_dir> <build_type>")
-        sys.exit(1)
+def install_python_requirements(workspace):
+    requirements_path = os.path.join(workspace, "python", "requirements.txt")
+    run_cmd([
+        sys.executable, "-m", "pip", "install", "-r", requirements_path])
 
-    install_dir = os.path.abspath(sys.argv[1])
-    temp_dir    = os.path.abspath(sys.argv[2])
-    build_type  = sys.argv[3]
+# ----------------------------------------------------------------------------------------------------------------------
+def main(args):
+    workspace    = os.path.dirname(os.path.abspath(__file__))
 
-    install_dir = os.path.join(install_dir, build_type)
+    # update submodules
+    print("-------------------------------------------------------------------")
+    print(f"Update submodules in workspace: {workspace}")
+    print("-------------------------------------------------------------------")
+    run_cmd(["git", "submodule", "update", "--init", "--recursive"], cwd=workspace)
 
-    setup_dep(install_dir, temp_dir,
-              "googletest", "https://github.com/google/googletest.git", "v1.17.0", build_type,
-              "-Dgtest_force_shared_crt=ON")
+    # install python dependencies
+    print("-------------------------------------------------------------------")
+    print(f"Install Python requirements in workspace: {workspace}")
+    print("-------------------------------------------------------------------")
+    install_python_requirements(workspace)
 
-    setup_dep(install_dir, temp_dir,
-              "spdlog", "https://github.com/gabime/spdlog.git", "v1.15.3", build_type)
+    # build third-party dependencies
+    print("-------------------------------------------------------------------")
+    print(f"Build third-party dependencies to the path: {workspace}")
+    print("-------------------------------------------------------------------")
+    build_cmake_depend(workspace, args.build_type,
+                       "googletest",
+                       "-Dgtest_force_shared_crt=ON")
+
+    build_cmake_depend(workspace, args.build_type,
+                       "spdlog")
 
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--build_type",
+                        type=str,
+                        choices=["Debug", "Release", "RelWithDebInfo"],
+                        default="RelWithDebInfo",
+                        help="Specify build type")
+    args = parser.parse_args()
+
+    main(args)
